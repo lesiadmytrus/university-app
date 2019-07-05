2f2e3cb2eda28bcfb0907e2c11040fc868ebd9df
Lesia Dmytrus <ldmyt@softserveinc.com>
2019-07-05 14:29:08 +0300

    Added call to API to get students

--- src/app/components/student/student.component.ts
+++ src/app/components/student/student.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Student } from 'src/app/models/student.model';
 import { STUDENTS } from '../../mock-students';
+import { StudentService } from 'src/app/services/student.service';
+import { first } from 'rxjs/operators';
 
 @Component({
   selector: 'app-student',
@@ -8,15 +10,21 @@ import { STUDENTS } from '../../mock-students';
   styleUrls: ['./student.component.scss']
 })
 export class StudentComponent implements OnInit {
-  students = STUDENTS;
+  students: Student[] = [];
   selectedStudent: Student;
 
-  constructor() { }
+  constructor(private studentService: StudentService) { }
 
   ngOnInit() {
+    this.getStudents();
   }
 
   onSelect(student: Student): void {
     this.selectedStudent = student;
+  }
+
+  getStudents(): void {
+    this.studentService.getAll()
+      .subscribe(students => this.students = students);
   }
 }
