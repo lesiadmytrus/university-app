35c58d97c301213e82e3c9ba45cb7d54f51c62dd
Lesia Dmytrus <ldmyt@softserveinc.com>
2019-07-05 13:04:10 +0300

    minor changes

--- src/app/services/student.service.ts
+++ src/app/services/student.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Student } from '../models/student.model';
 
 @Injectable({ providedIn: 'root' })
 export class StudentService {
-  apiURL = 'http://localhost:3000/api/students';
+   private apiURL = 'http://localhost:3000/api/students';
 
   constructor(private http: HttpClient) {}
 
-  public createStudent(student) {
-    return this.http.post(this.apiURL, student);
+  public createStudent(student: Student): Observable<Student> {
+    return this.http.post<Student>(this.apiURL, student);
   }
 }
