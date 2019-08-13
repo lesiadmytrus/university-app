8cf7d537d2a3df914f93d3243ca706af6c4d6376
Lesia Dmytrus <ldmyt@softserveinc.com>
2019-08-13 10:16:30 +0300

    Added content to home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { StudentFormComponent } from './components/student-form/student-form.component';
 import { StudentComponent } from './components/student/student.component';
+import { HomeComponent } from './components/home/home.component';
 
 const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
   { path: 'students', component: StudentComponent },
   { path: 'students/create', component: StudentFormComponent },
   { path: 'students/:id/edit', component: StudentFormComponent }
