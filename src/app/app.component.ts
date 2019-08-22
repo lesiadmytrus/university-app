3b42301764f826c2617e2d7e63c49506a0553925
Lesia Dmytrus <ldmyt@softserveinc.com>
2019-08-22 14:19:07 +0300

    Added log in functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../app/services/auth.service';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +9,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  isLoggedIn = false;
+
+  constructor(
+    private auth: AuthService,
+    private router: Router
+  ) {
+    this.router.events
+      .pipe(
+        filter(event => event instanceof NavigationEnd)
+      )
+        .subscribe(event => {
+          this.isLoggedIn = this.auth.isLoggedIn();
+        });
+    }
+
+  onLogOut(): void {
+    this.auth.logOut();
+    this.router.navigate(['/login']);
+  }
 }
