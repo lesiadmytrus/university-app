655d4b1db26c842f12b64076b0a05663228cfb36
Lesia Dmytrus <ldmyt@softserveinc.com>
2019-08-22 14:12:08 +0300

    Added user model

---
+++ src/app/models/user.model.ts
@@ -0,0 +1,9 @@
+export class User {
+    public _id: string;
+    public firstName: string;
+    public lastName: string;
+    public email: string;
+    public country: string;
+    public password: string;
+    public confirmPassword: string;
+}
