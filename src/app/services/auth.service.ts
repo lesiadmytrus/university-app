b6fe6ea3be31f572801e8209c9c18498c2dace6f
Lesia Dmytrus <ldmyt@softserveinc.com>
2019-08-22 14:22:08 +0300

    Added auth service for logIn & logOut components

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../models/user.model';
+import { tap } from 'rxjs/operators';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AuthService {
+    
+	constructor(private http: HttpClient) { }
+
+	private readonly apiURL = 'http://localhost:3000/api/auth';
+
+	public registerUser(user: User): Observable<Object> {
+		return this.http.post(`${this.apiURL}/register`, user);
+	}
+
+	public logInUser(email: string, password: string): Observable<{access_token: string}> {
+		return this.http.post<{access_token: string}>(`${this.apiURL}/login`, { email, password })
+			.pipe(
+				tap(res => {
+					this.setToken(res.access_token);
+				})
+			);
+	}
+
+	public setToken(token: string) {
+		localStorage.setItem('access_token', token);
+	}
+
+	public getToken() {
+		return localStorage.getItem('access_token');
+	}
+
+	public isLoggedIn() {
+		return !!(this.getToken());
+	}
+
+	public logOut(): void {
+		localStorage.removeItem('access_token');
+	}
+}
