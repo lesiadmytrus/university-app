a0c7ee5958708b8ad9afeb36edc359ab80b38344
Lesia Dmytrus <ldmyt@softserveinc.com>
2019-08-22 14:20:47 +0300

    Added logIn & logOut routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,17 @@ import { Routes, RouterModule } from '@angular/router';
 import { StudentFormComponent } from './components/student-form/student-form.component';
 import { StudentComponent } from './components/student/student.component';
 import { HomeComponent } from './components/home/home.component';
+import { RegistrationComponent } from './components/registaration/registration.component';
+import { LoginComponent } from './components/login/login.component';
+import { AuthGuard } from './guards/auth-guard.service';
 
 const appRoutes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: 'students', component: StudentComponent },
-  { path: 'students/create', component: StudentFormComponent },
-  { path: 'students/:id/edit', component: StudentFormComponent }
+  { path: 'students', component: StudentComponent, canActivate: [AuthGuard] },
+  { path: 'students/create', component: StudentFormComponent, canActivate: [AuthGuard] },
+  { path: 'students/:id/edit', component: StudentFormComponent, canActivate: [AuthGuard] },
+  { path: 'registration', component: RegistrationComponent },
+  { path: 'login', component: LoginComponent }
 ];
 
 @NgModule({
