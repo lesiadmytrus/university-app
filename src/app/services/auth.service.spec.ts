09653733dc79a8bc00a05fd57b39a22791ccd14a
Lesia Dmytrus <ldmyt@softserveinc.com>
2019-09-05 11:06:39 +0300

    Added unit test to auth service

---
+++ src/app/services/auth.service.spec.ts
@@ -0,0 +1,57 @@
+import { TestBed } from '@angular/core/testing';
+
+import { AuthService } from '../services/auth.service';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpTestingController, HttpClientTestingModule } from '@angular/common/http/testing';
+
+const mockToken = 'fkjh45hk45jdhfg546456h56hjgkljdgj';
+
+describe('AuthService', () => {
+  let auth: AuthService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ],
+      imports: [
+        HttpClientModule,
+        HttpClientTestingModule
+      ],
+      providers: [
+        { provide: AuthService, useClass: AuthService }
+      ]
+    });
+
+    auth = TestBed.get(AuthService);
+  });
+
+  it('should be created', () => {
+    expect(auth).toBeTruthy();
+  });
+
+  describe('LocalStorage', () => {
+    it('#setItem should set user token', () => {
+      spyOn(localStorage, 'setItem').and.callThrough();
+      auth.setToken(mockToken);
+      expect(localStorage.setItem).toHaveBeenCalled();
+    });
+
+    it('#getItem should get user token', () => {
+      spyOn(localStorage, 'setItem').and.callThrough();
+      spyOn(localStorage, 'getItem').and.callThrough();
+      auth.getToken();
+      expect(localStorage.getItem).toHaveBeenCalledWith('access_token');
+    });
+
+    it('#removeItem should delete user token', () => {
+      spyOn(localStorage, 'removeItem').and.callThrough();
+      auth.logOut();
+      expect(localStorage.removeItem).toHaveBeenCalledWith('access_token');
+    });
+
+    it('should return true if user is log in', () => {
+      spyOn(localStorage, 'getItem').and.callThrough();
+      auth.isLoggedIn();
+      expect(localStorage.getItem).toHaveBeenCalledWith('access_token');
+    });
+  });
+});
